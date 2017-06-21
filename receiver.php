2ab70f49ce3397aca5430d92d804d6648ada9b82
1Cham <neenan.lee@gmail.com>
2017-06-21 16:38:45 +0930

    Update receiver.php

--- receiver.php
+++ receiver.php
@@ -1,7 +1,20 @@
 <?php
+$dsn = getenv('MYSQL_DSN');
+$user = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+if (!isset($dsn, $user) || false === $password){
+	throw new Exception('Set MYSQL_DSN,MYSQL_USER, and MYSQL_PASSWORD environment variables');
+}
+$db = new PD0($dsn, $user, $password);
 
-$my_file = 'file.txt';
-$handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
+$statement = $db->prepare("select messageaksndlkan");
+$statement->execute();
+$all = $statement->fetchAll();
+
+foreach ($all as $data){
+	echo $data["message"]."<br>";
+}
+
 $data = $_GET['lat'].", ".$_GET['long'];
 fwrite($handle, $data);
 fclose($handle);
