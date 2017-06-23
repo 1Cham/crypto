57ce3f18b6378fd51d93d5d5d6a093eea6db27bd
1Cham <neenan.lee@gmail.com>
2017-06-23 18:36:16 +0930

    Update receiver.php

--- receiver.php
+++ receiver.php
@@ -5,11 +5,12 @@ $password = getenv('MYSQL_PASSWORD');
 if (!isset($dsn, $user) || false === $password){
 	throw new Exception('Set MYSQL_DSN,MYSQL_USER, and MYSQL_PASSWORD environment variables');
 }
-$db = new PD0($dsn, $user, $password);
-if(isset($_POST['long'])&isset($_POST['lat'])&isset($_POST['speed']){
-	$statement = $db->prepare("INSERT INTO location (vehicleid,longitude,latitude,speed) VALUES (1,'".$_POST['long']."','".$_POST['lat']."',".$_POST['speed'].")");
-	$statement->execute();
-}
+
+//$db = new PD0($dsn, $user, $password);
+//if(isset($_POST['long'])&isset($_POST['lat'])&isset($_POST['speed']){
+//	$statement = $db->prepare("INSERT INTO location (vehicleid,longitude,latitude,speed) VALUES (1,'".$_POST['long']."','".$_POST['lat']."',".$_POST['speed'].")");
+//	$statement->execute();
+//}
 //$all = $statement->fetchAll();
 
 
